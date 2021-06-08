73d2d6e87b827306c8d1f4fbccb989a4e8a34a38
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-06-08 17:34:31 -0500

    reference-type-tests

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,7 +6,46 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
-        public void Test1()
+        public void CSharpIsPassByValue()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+            // act
+
+
+            // assert
+            Assert.Equal("New Name", book1.Name);
+
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+            // act
+
+
+            // assert
+            Assert.Equal("New Name", book1.Name);
+            
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
         {
             // arrange
             var book1 = GetBook("Book 1");
@@ -17,6 +56,21 @@ namespace GradeBook.Tests
             // assert
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+            // act
+
+
+            // assert
+            Assert.Same(book1, book2);
+            
         }
 
         Book GetBook(string name)
