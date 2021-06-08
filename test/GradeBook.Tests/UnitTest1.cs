80afed7d6611abb5108e042675ae49f0fd6d0803
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-06-08 16:30:04 -0500

    refactor-code

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -8,7 +8,19 @@ namespace GradeBook.Tests
         [Fact]
         public void Test1()
         {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(90.5);
+            book.AddGrade(100);
+            book.AddGrade(75);
 
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(85.5, result.Average);
+            Assert.Equal(85.5, result.High);
+            Assert.Equal(85.5, result.Average);
         }
     }
 }
