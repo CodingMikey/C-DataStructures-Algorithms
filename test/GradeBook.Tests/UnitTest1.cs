b83efa615b72f12477b48c14032a2ed3d6e68217
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-06-08 16:54:38 -0500

    creating-solution-file

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -10,17 +10,17 @@ namespace GradeBook.Tests
         {
             // arrange
             var book = new Book("");
+            book.AddGrade(89.1);
             book.AddGrade(90.5);
-            book.AddGrade(100);
-            book.AddGrade(75);
+            book.AddGrade(77.3);
 
             // act
             var result = book.GetStatistics();
 
             // assert
-            Assert.Equal(85.5, result.Average);
-            Assert.Equal(85.5, result.High);
-            Assert.Equal(85.5, result.Average);
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
         }
     }
 }
