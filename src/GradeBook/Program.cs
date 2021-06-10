5ae8b87c6905e8293c441669fabd0cd01d3937b4
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-06-10 10:24:24 -0700

    try-catch

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,15 +8,35 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Mikes Grade Book");
-            book.AddGrade(90.5);
-            book.AddGrade(90);
-            book.AddGrade(77);
+            
+
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
